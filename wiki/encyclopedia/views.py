c49bbaab2eae518a87d553f724e8efb170d48493
mero313 <ameer_mazin2001@yahoo.com>
2023-09-15 01:32:36 +0300

    desk

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -44,6 +44,37 @@ def search(request):
             "title": search ,
             "content": html_content 
         })
+        else:
+            allEntries = util.list_entries()
+            counter = []
+            for entry in allEntries:
+                if search.lower() in entry.lower():
+                    counter.append(entry)
+                    return render (request ,'encyclopedia/search.html' ,{
+                        'counter': counter
+                    })
+                
+        
+def new_page(request):
+    if request.method =="GET":
+        return render(request , "encyclopedia/new.html" )
+    else:
+        title= request.POST['title']
+        content = request.POST['content']
+        titleExist =util.get_entry(title)
+        if titleExist is not None:
+            return render (request , "encyclopedia/error.html" , {
+                "message": "Entry page already exists"
+            })
+        else:
+            util.save_entry(title , content)
+            html_content = convert_html(title)
+            return render(request , "encyclopedia/entry.html" , {
+                "title":title , 
+                "content": html_content
+            })
+
+
 
         
 
